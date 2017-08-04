af11956ee05f1b030781a68fb2e5b31de0bf3d42
zsf <852333743@qq.com>
2017-08-04 13:29:33 +0800

    添加ImageLoader类

---
+++ SfImageLoader/app/src/main/java/com/zsf/sfimageloader/image/ImageLoader.java
@@ -0,0 +1,78 @@
+package com.zsf.sfimageloader.image;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by zsf on 2017/8/4.
+ * 图片加载类
+ * 要求：实现图片加载，并且要将图片缓存起来
+ */
+
+public class ImageLoader {
+    //图片缓存
+    LruCache<String,Bitmap> mImageCache;
+    //线程池，线程数量为CPU数量
+    ExecutorService mExecuroService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public ImageLoader(){
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        //计算可使用的最大内存
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        //取四分之一的可用内存作为缓存
+        final int cacheSize = maxMemory / 4;
+        mImageCache = new LruCache<String,Bitmap>(cacheSize){
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+    }
+
+
+    public void displayImage(final String url, final ImageView imageView){
+        imageView.setTag(url);
+        mExecuroService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null){
+                    return;
+                }
+                if (imageView.getTag().equals(url)){
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url,bitmap);
+            }
+        });
+    }
+
+    public Bitmap downloadImage(String imageUrl){
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            connection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+}
